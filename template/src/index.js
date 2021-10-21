08dbea31a2511e3be7419d484be6184656311fbc
Dan Filler <dwfiller@gmail.com>
2021-10-20 23:54:50 -0400

    Index files, gitignore build

---
+++ template/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Site = () => {
+  return <>Template Site</>
+}
+
+
+ReactDOM.render(<Site />, document.getElementById("root"));
